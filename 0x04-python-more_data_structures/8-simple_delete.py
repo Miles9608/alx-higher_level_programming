f7dedcc36679cb86911c854556799fdd2ef3a726
Simbarashe Lungu <visualnerds96@gmail.com>
2023-08-17 05:56:50 -0700

    8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    return {k: v * 2 for k, v in a_dictionary.items()}
+    return {k: v * 2 for k, v in a_dictionary.items()}#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    a_dictionary.pop(key, None)
+    return a_dictionary
